cb8369bd8929c5d3715bb4f919f4e6a190296503
gaurivn <gaurin391@gmail.com>
2019-05-14 23:34:51 +0400

    Second try

---
+++ JuliaGPU/session1/Gauri/q1.jl
@@ -0,0 +1,13 @@
+#1. Hamming Distance of 2 no.s
+function HammingDist(s1,s2)
+  res = 0
+  x = xor(s1,s2)
+  while(x>0)
+    res = res + (x&1)
+    x = x>>>1
+  end
+  println("Hamming Distance = $res")
+end
+s1=readline()
+s2=readline()
+HammingDistance(parse(Int64,s1),parse(Int64,s2))
