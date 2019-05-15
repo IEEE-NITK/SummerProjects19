c483d4cc2ada673488ea1d5cf3820c6f1fa50aef
NegaMage <feyazabubaker@gmail.com>
2019-05-15 21:42:09 +0530

    Making one last change!

--- JuliaGPU/session1/Feyaz/qn3.jl
+++ JuliaGPU/session1/Feyaz/qn3.jl
@@ -1,9 +1,14 @@
-println("ONLY ASCII PLS. NO INPUT VERIFICATION ฅ(=^･ω･^=)ฅ∫ ")
+#println("ONLY ASCII PLS. NO INPUT VERIFICATION ฅ(=^･ω･^=)ฅ∫ ")
 
 q= readline()
 #println(typeof(q)):: string
 
-for x in q
-	print(Int(x))
+ans=""
+temp=""
+len=lastindex(q)
+for i in 1:len
+	#println(string(Int32(q[i]),base=16))
+	temp=string(Int32(q[i]),base=16)
+	global ans= ans*temp
 	end
-println()
+println(ans)
