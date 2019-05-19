5e19b5f82195cac95f5792e5e7c16ac3bccc7965
Manav Singhal <42416903+SidDsloth@users.noreply.github.com>
2019-05-19 11:21:13 +0530

    Update q1.jl

--- JuliaGPU/session1/Manav Singhal/q1.jl
+++ JuliaGPU/session1/Manav Singhal/q1.jl
@@ -3,8 +3,9 @@ function dectobin(no)
 	no1=0
 	while no!=0
 		remai=no%2
-		no1=remai*(10^coun)
-		no=Int32(no/2)
+		no1=no1+remai*(10^coun)
+		coun=coun+1
+		no=Int(floor(no/2))
 	end
 	return no1
 end
@@ -13,14 +14,12 @@ number2=parse(UInt8, readline())
 ans=xor(number1,number2)
 c=0
 ans=dectobin(ans)
-println(ans)
 while ans!=0
 	global remain=ans % 10
 	if remain==1
 		global c=c+1
 	end
-	global ans= Int32(ans/10)
+	global ans= Int(floor(ans/10))
 end
 println("Hamming Distance: ",c)
 	
-
