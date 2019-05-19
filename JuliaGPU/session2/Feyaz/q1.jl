75f860d45febc51e45d794571343b9c69f70048d
NegaMage <feyazabubaker@gmail.com>
2019-05-19 22:04:45 +0530

    quicksort julia assignment 2 now works properly for rando data

--- JuliaGPU/session2/Feyaz/q1.jl
+++ JuliaGPU/session2/Feyaz/q1.jl
@@ -1,39 +1,40 @@
-
-#I tried it twice, and built in sort function was around 67+ ms, 
-# and insertion sort was around 1 ms.
 using BenchmarkTools
 arr = rand(Int, 1048576)    #2^20=1048576
 len = lastindex(arr)
 
-function quicksort!(array)
-    if length(array) > 1
-        left=firstindex(array)
-        right=lastindex(array)
-
-        pivot= array[trunc(Int64,(left+right)/2)]
-        while left ≤ right
+function quicksort!(array,low=1,high=length(array))
+    if high > low
+        pivot = array[rand(low:high)]
+        left, right = low, high
+        while left <= right
             while array[left] < pivot
-                left= left + 1
+                left += 1
             end
             while array[right] > pivot
-                right= right - 1
+                right -= 1
             end
-            if left ≤ right
-                array[left], array[right]=array[right], array[left]
-                left= left + 1
-                right= right - 1
+            if left <= right
+                array[left], array[right] = array[right], array[left]
+                left += 1
+                right -= 1
             end
         end
-        l=firstindex(array)
-        h=lastindex(array)
-        quicksort!(array[l:right])
-        quicksort!(array[left:h])
-    else
-        return(array)
+        quicksort!(array,low,right)
+        quicksort!(array,left,high)
     end
+    return array
 end
 
-println("Built in sort function")
-println(@benchmark sort(arr))
-println("Quicksort implementation")
-println(@benchmark quicksort!(arr))
+#println("Built in sort function")
+#println(@benchmark sort(arr))
+#println("Quicksort implementation")
+#println(@benchmark quicksort!(arr))
+a=rand(Int, 20)
+for x in a
+	println(x)
+end
+println("NEW LINE")
+quicksort!(a)
+for x in a
+	println(x)
+end
