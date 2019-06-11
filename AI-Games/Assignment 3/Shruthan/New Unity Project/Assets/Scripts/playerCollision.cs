69b722000d4045842e99c06fcdf94ded3f9f82bf
shruthanr <shruthan@outlook.com>
2019-06-11 15:24:29 +0530

    AI-Games Assignment-3 Shruthan (recloned)

---
+++ AI-Games/Assignment 3/Shruthan/New Unity Project/Assets/Scripts/playerCollision.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+    public PlayerScript movement;
+    
+    private void OnCollisionEnter(Collision collisioninfo)
+    {
+        
+        if (collisioninfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+}
