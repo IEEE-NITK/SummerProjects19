6bae30ab6fd3327b2bf47a4bd6a78343a81bd3f4
ansen1701 <ansen2061@gmail.com>
2019-06-13 23:16:36 +0530

    Assignment 3 - Ansen

--- Cryptopals/Narayan/challenge1 set1.py
+++
@@ -1,7 +0,0 @@
-import binascii,base64
-
-print("Enter the hexadecimal string:")
-x=input()
-x_binary=binascii.unhexlify(x)
-x_base64=base64.b64encode(x_binary).decode('utf8')
-print(x_base64)
--- JuliaGPU/session1/Animesh/q3.jl
+++
@@ -1,6 +0,0 @@
-n = readline(stdin)
-i =1
-while i <=length(n)
-  print(string(Int(n[i]),base=16))
-  global i = i+1
-end
